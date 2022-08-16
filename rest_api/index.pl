dfe5056abd7cf87025492e40ff1b0a6fbbafdf48
Hevanafa <dhevansafa4@hotmail.com>
2022-08-17 06:25:23 +0700

    tidying up templates

--- rest_api/index.pl
+++ rest_api/index.pl
@@ -1,29 +1,34 @@
 # this requires Mojolicious
 # install with: cpanm Mojolicious
+
+# also make it easier to develop with Mojolicious VSCode extension, which
+# supports syntax highlighting for template files
+
+# requires Perl::Tidy to format the code
+# install with: cpanm Perl::Tidy
+
 # start with: perl .\index.pl daemon -m production -l http://*:8080
+# more information: https://docs.mojolicious.org/Mojolicious/Lite
 
 use Mojolicious::Lite -signatures;
 
+# basic parametrised routing
 get "/:foo" => sub ($c) {
     my $foo = $c->param("foo");
-    $c->render(text => "Hello from $foo.");
+    $c->render( text => "Hello from $foo." );
 };
 
-get '/' => sub ($c) {
-  $c->render(template => 'index');
+# JSON output example with hash reference
+get "/questions/:question_id" => sub {
+    my $self   = shift;
+    my $result = { question_id => int $self->param("question_id") };
+    return $self->render( json => $result );
+};
+
+get '/' => sub {
+    my $self = shift;
+    # render templates\index.html.ep
+    $self->render( template => 'index' );
 };
 
 app->start;
-__DATA__
-
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
